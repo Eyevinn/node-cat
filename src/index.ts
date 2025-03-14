7165c244a33b2b3e8dc17e482db1921adae8a985
Jonas Birmé <jonas.birme@eyevinn.se>
2025-03-15 00:58:00 +0100

    chore: documentation

--- src/index.ts
+++ src/index.ts
@@ -15,11 +15,47 @@ export interface CatGenerateOptions {
   kid: string;
 }
 
+/**
+ * Options for the CAT object
+ *
+ * @param {Object} keys - Key object
+ * @param {boolean} expectCwtTag - Expect CWT tag in token
+ *
+ * @example
+ * const opts = {
+ *   keys: {
+ *     Symmetric256: Buffer.from('403697de87af64611c1d32a05dab0fe1fcb715a86ab435f1ec99192d79569388', 'hex')
+ *   },
+ *   expectCwtTag: true
+ * };
+ */
 export interface CatOptions {
   keys: { [keyid: string]: Buffer };
   expectCwtTag?: boolean;
 }
 
+/**
+ * Common Access Token (CAT) validator and generator
+ *
+ * @param {CatOptions} opts - Options for the CAT object
+ *
+ * @example
+ * const validator = new CAT({
+ *   keys: {
+ *     Symmetric256: Buffer.from(
+ *       '403697de87af64611c1d32a05dab0fe1fcb715a86ab435f1ec99192d79569388',
+ *       'hex'
+ *     )
+ *   },
+ * });
+ * try {
+ *   await validator.validate(base64encoded, 'mac', 'coap://as.example.com', {
+ *     kid: 'Symmetric256'
+ *   });
+ * } catch (e) {
+ *   // Not valid, handle error
+ * }
+ */
 export class CAT {
   private keys: { [keyid: string]: Buffer };
   private expectCwtTag = false;
