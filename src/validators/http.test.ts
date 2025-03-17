b926b663667b86d3d9d17a21ee357dfd2a0abb8d
Jonas Birmé <jonas.birme@eyevinn.se>
2025-03-17 01:30:41 +0100

    chore: test response from http validator when key is wrong

--- src/validators/http.test.ts
+++ src/validators/http.test.ts
@@ -2,6 +2,35 @@ import { createRequest } from 'node-mocks-http';
 import { HttpValidator } from './http';
 
 describe('HTTP Request CAT Validator', () => {
+  test('fail to validate token in CTA-Common-Access-Token header with wrong signature', async () => {
+    const request = createRequest({
+      method: 'GET',
+      headers: {
+        'CTA-Common-Access-Token':
+          '2D3RhEOhAQWhBFBha2FtYWlfa2V5X2hzMjU2U6MEGnUCOrsGGmfXRKwFGmfXRKxYIOM6yRx830uqAamWFv1amFYRa5vaV2z5lIQTqFEvFh8z'
+      }
+    });
+    const httpValidator = new HttpValidator({
+      keys: [
+        {
+          kid: 'Symmetric256',
+          key: Buffer.from(
+            '403697de87af64611c1d32b05dab0fe1fcb715a86ab435f1ec99192d79569388',
+            'hex'
+          )
+        }
+      ],
+      issuer: 'eyevinn'
+    });
+    const result = await httpValidator.validateHttpRequest(
+      request,
+      'Symmetric256'
+    );
+    expect(result.status).not.toBe(200);
+    expect(result.status).toBe(401);
+    expect(result.message).toBe('Tag mismatch');
+  });
+
   test('can validate token in CTA-Common-Access-Token header', async () => {
     const request = createRequest({
       method: 'GET',
